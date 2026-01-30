b544754b1f5273381110bd2d4e2b66d3772a9f13
kvjohnny <kvjohnny.van@gmail.com>
2026-01-30 09:23:51 +0200

    fixed file in kt-calculate-total-meal-cost task

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,11 @@
-// create a function and implement it
+fun calculateTotalMealCost(
+    baseCost: Double,
+    taxRate: Double,
+    tipPercentage: Double,
+    discount: Double = 0.0
+): Double {
+    fun applyTax(): Double = baseCost * taxRate
+    fun applyTip(): Double = (baseCost + applyTax()) * tipPercentage
+    fun applyDiscount(): Double = discount
+    return baseCost + applyTax() + applyTip() - applyDiscount()
+}
